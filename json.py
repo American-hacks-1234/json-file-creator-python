a30f7bc5de11dafbb7d6942d5017517e8442b00c
American Awesome Sauce Software <russelltm1@s.dcsdk12.org>
2026-01-20 12:12:44 -0700

    Implement logging in json.py
    Added logging functionality to json.py

---
+++ json.py
@@ -0,0 +1,20 @@
+import time
+import json
+
+# Config files
+TAMPER_LOG = "My.log"
+#msg to put into log
+msg="Logged. (You can change this msg at anytime)"
+
+
+def now_ts():
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+
+def append_my_log(msg):
+    try:
+        with open(TAMPER_LOG, "a", encoding="utf-8") as fh:
+            fh.write(f"{now_ts()} - {msg}\n")
+    except Exception:
+        pass
+append_my_log(msg)
+now_ts()
